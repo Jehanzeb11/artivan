48b2789276d5b10792c96512736b8b15a387f7a4
jehanzeb <jehanzebsiddiqui287@gmail.com>
2024-08-06 17:20:14 +0500

    about continues

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Hero from '@/Components/about/Hero'
 import Experience from '@/Components/about/Experience'
+import QualityDesign from '@/Components/about/QualityDesign'
+import OurTeam from '@/Components/about/OurTeam'
+import Testimonials from '@/Components/Testimonials'
 
 const About = () => {
   return (
     <>
     <Hero />
     <Experience />
+    <QualityDesign />
+    <OurTeam />
+    <Testimonials/>
     </>
   )
 }
