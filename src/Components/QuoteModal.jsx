1246d8a492ee60698c635c02aedd7f6257d038fc
jehanzeb <jehanzebsiddiqui287@gmail.com>
2024-08-13 18:17:01 +0500

    modal bug fix

--- src/Components/QuoteModal.jsx
+++ src/Components/QuoteModal.jsx
@@ -10,7 +10,7 @@ export default function QuoteModal({ showModal, setShowModal }) {
           <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative max-sm:w-full w-auto max-sm:mx-2 my-6 mx-auto max-w-4xl">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none bg-my-gradient">
+              <form onSubmit={(e) => e.preventDefault()} className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none bg-my-gradient">
                 {/*header*/}
                 <div className="flex items-start justify-between px-5 pt-5 rounded-t">
                   <h3 className="text-2xl text-gray-200 font-semibold">Get A Quote</h3>
@@ -18,7 +18,7 @@ export default function QuoteModal({ showModal, setShowModal }) {
                 </div>
                 {/*body*/}
                 <div className="relative p-1 flex-auto ">
-                <form onSubmit={(e) => e.preventDefault()} className=" p-4 rounded-lg">
+                <div className=" p-4 rounded-lg">
 
 <div className="text-white grid grid-cols-12 lg:gap-3 gap-1.5">
     <div className="sm:col-span-6 col-span-12 flex flex-col">
@@ -42,7 +42,7 @@ export default function QuoteModal({ showModal, setShowModal }) {
     </div>
 
 </div>
-</form>
+</div>
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end pb-4 pr-4 rounded-b">
@@ -55,13 +55,13 @@ export default function QuoteModal({ showModal, setShowModal }) {
                   </button>
                   <button
                     className="py-1.5 px-4 font-medium rounded-lg bg-gray-100 text-gray-800"
-                    type="button"
-                    onClick={() => setShowModal(false)}
+                    type="submit"
+                    // onClick={() => setShowModal(false)}
                   >
                     Submit
                   </button>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
           <div className="opacity-40 fixed inset-0 z-40 bg-black"></div>
